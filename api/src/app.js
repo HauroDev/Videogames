5d2d10253a4ddc3e35d8f880e1b631b969f2986b
HauroEnojado <victormanu_el08@outlook.com>
2023-05-15 14:43:26 -0300

    no entiendo nada XD

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,7 @@ server.use(bodyParser.json({ limit: '50mb' }))
 server.use(cookieParser())
 server.use(morgan('dev'))
 server.use((_, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://videogames-pi-soy-henry.netlify.app') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://videogames-pi-soy-henry.netlify.app','http://localhost:4173') // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header(
     'Access-Control-Allow-Headers',
