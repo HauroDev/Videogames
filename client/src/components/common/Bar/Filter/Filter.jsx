1be035f39a2bd5b3b83a718082b884655c0a1997
HauroEnojado <victormanu_el08@outlook.com>
2023-05-20 20:57:04 -0300

    fixeando bugs visuales parte 2

--- client/src/components/common/Bar/Filter/Filter.jsx
+++ client/src/components/common/Bar/Filter/Filter.jsx
@@ -61,7 +61,7 @@ const Filter = () => {
             </div>
             <div className={styles.option}>
               <label htmlFor='genres'>Generos:</label>
-              <select name='genres' onChange={addGenre} multiple>
+              <select name='genres' onClick={addGenre} onBlur={addGenre} multiple>
                 {genres?.map((g) => (
                   <option key={g.id} value={g.id}>
                     {g.name}
